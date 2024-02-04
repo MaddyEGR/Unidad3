00b7a0ff2d9fc813063010bed616eae6fb793d70
Madheline Gonzalez <madheline.gonzalez@galileo.edu>
2024-02-04 17:29:37 -0600

    First Commit

---
+++ Signo Zodiacal1.js
@@ -0,0 +1,124 @@
+var mes = prompt("Ingrese su mes de nacimiento en numeros");
+var dia = prompt("Ingrese su dia de nacimiento en numeros");
+
+if (mes ==1){
+  if (dia <=20){
+    alert("Usted es Capricornio")
+  }
+}
+    if (mes ==1){
+      if (dia >= 21){
+        alert("Usted es Acuario")
+  }
+}
+
+if (mes ==2){
+  if (dia <=19){
+    alert("Usted es Acuario")
+  }
+}
+    if (mes ==2){
+      if (dia >= 20){
+        alert("Usted es Piscis")
+  }
+}
+if (mes ==3){
+  if (dia <=20){
+    alert("Usted es Piscis")
+  }
+}
+    if (mes ==3){
+      if (dia >= 21){
+        alert("Usted es Aries")
+  }
+}
+if (mes ==4){
+  if (dia <=21){
+    alert("Usted es Aries")
+  }
+}
+    if (mes ==4){
+      if (dia >= 21){
+        alert("Usted es Tauro")
+  }
+}
+if (mes ==5){
+  if (dia <=20){
+    alert("Usted es Tauro")
+  }
+}
+    if (mes ==5){
+      if (dia >= 21){
+        alert("Usted es Geminis")
+  }
+}
+if (mes ==6){
+  if (dia <=20){
+    alert("Usted es Geminis")
+  }
+}
+    if (mes ==6){
+      if (dia >= 21){
+        alert("Usted es Cancer")
+  }
+}
+if (mes ==7){
+  if (dia <=22){
+    alert("Usted es Cancer")
+  }
+}
+    if (mes ==7){
+      if (dia >= 23){
+        alert("Usted es Leo")
+  }
+}
+if (mes ==8){
+  if (dia <=23){
+    alert("Usted es Leo")
+  }
+}
+    if (mes ==1){
+      if (dia >= 24){
+        alert("Usted es Virgo")
+  }
+}
+if (mes ==9){
+  if (dia <=22){
+    alert("Usted es Virgo")
+  }
+}
+    if (mes ==9){
+      if (dia >= 23){
+        alert("Usted es Libra")
+  }
+}
+if (mes ==10){
+  if (dia <=23){
+    alert("Usted es Libra")
+  }
+}
+    if (mes ==10){
+      if (dia >= 24){
+        alert("Usted es Escorpio")
+  }
+}
+if (mes ==11){
+  if (dia <=22){
+    alert("Usted es Escorpio")
+  }
+}
+    if (mes ==11){
+      if (dia >= 23){
+        alert("Usted es Sagitario")
+  }
+}
+if (mes ==12){
+  if (dia <=21){
+    alert("Usted es Sagitario")
+  }
+}
+    if (mes ==1){
+      if (dia >= 22){
+        alert("Usted es Capricornio")
+  }
+}
